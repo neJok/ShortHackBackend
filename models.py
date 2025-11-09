540848d1d6f4570cb325166115252a6c8989cc0f
Kostya <pavlenkkovkosta@gmail.com>
2025-11-09 12:42:37 +0300

    add application functional

--- models.py
+++ models.py
@@ -1,28 +1,31 @@
 from pydantic import BaseModel, Field
 from typing import Optional, List
 from datetime import datetime
+from bson import ObjectId
 
 class User(BaseModel):
-    id: Optional[int] = None
+    id: Optional[str] = Field(default_factory=lambda: str(ObjectId()), alias="_id")
     full_name: str
     email: str
     hashed_password: str
     role: str = "student"  # student, curator, admin
 
 class Room(BaseModel):
-    id: int
+    id: Optional[str] = Field(default_factory=lambda: str(ObjectId()), alias="_id")
     name: str
     capacity: int
 
 class EventApplication(BaseModel):
-    id: int
+    id: Optional[str] = Field(default_factory=lambda: str(ObjectId()), alias="_id")
     title: str
     description: str
     start_time: datetime
     end_time: datetime
-    organizer_id: int
-    room_id: int
-    status: str = "pending" # pending, approved, rejected
+    organizer_id: str
+    room_id: Optional[int] = None
+    status: str = "pending"  # pending, approved, rejected
+    assigned_room_id: Optional[int] = None
+    curator_comment: Optional[str] = None
 
 class UserCreate(BaseModel):
     full_name: str
