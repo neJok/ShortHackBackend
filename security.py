1e882d1deac7158c40fa0ec3e40d670995e2d9bf
Kostya <pavlenkkovkosta@gmail.com>
2025-11-09 12:16:28 +0300

    fix all deploy bugs

--- security.py
+++ security.py
@@ -23,7 +23,7 @@ def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password):
-    return pwd_context.hash(password[:72])
+    return pwd_context.hash(password)
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
