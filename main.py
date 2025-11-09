3e2c31e90701644cb3f6470d75318c342992d038
Kostya <pavlenkkovkosta@gmail.com>
2025-11-09 12:10:08 +0300

    fix all deploy bugs

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from auth import router as auth_router
 
 app = FastAPI(
     title="Univent API",
     version="0.1.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
