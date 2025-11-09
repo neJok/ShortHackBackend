64354beacaabcb7fd3b1fe7a4ec9d7a9ad8a189c
Kostya <pavlenkkovkosta@gmail.com>
2025-11-09 13:01:05 +0300

    fix

--- auth.py
+++ auth.py
@@ -30,7 +30,7 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     if not user or not verify_password(form_data.password, user["hashed_password"]):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
+            detail="Incorrect email or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
     access_token = create_access_token(data={"sub": str(user["_id"])})
